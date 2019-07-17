73de82debc89b079f25c1b108b3e5e8f53547ffe
Weerapong Pumngern <weerapong.pn@gmail.com>
2019-07-17 19:15:21 +0700

    add and modify files from django tutorial EP19-EP24

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,10 +3,16 @@ from django.conf.urls import url
 from music import views
 #from . import view
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 
     # /music/<album_id>
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
 ]
